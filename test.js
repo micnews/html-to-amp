6265960e8936a87242da502568fa04deb866a1b0
David Björklund <david.bjorklund@gmail.com>
2016-02-17 18:29:32 -0500

    add image dimensions

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import test from 'ava';
 import 'babel-core/register';
 import setupHtmlToAmp from './lib';
+import http from 'http';
+import Promise from 'pinkie-promise';
 
 const htmlToAmp = setupHtmlToAmp();
 
@@ -14,6 +16,27 @@ test('simple input', t => {
 test('handle image with size', t => {
   const html = '<img width="100" height="200" src="http://image.com" />';
   const expected = '<article><figure><amp-img width="100" height="200" layout="responsive" src="http://image.com"></amp-img></figure></article>';
+
+  return htmlToAmp(html).then(amp => { t.is(amp, expected); });
+});
+
+test('handle image without size', t => {
+  return new Promise(resolve => {
+    const server = http.createServer((req, res) => {
+      res.end(new Buffer('R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==', 'base64'));
+    }).listen(0, () => {
+      resolve(server.address().port);
+    });
+  }).then(port => {
+    const expected = `<article><figure><amp-img width="1" height="1" layout="responsive" src="http://localhost:${port}/"></amp-img></figure></article>`;
+    const html = `<img src="http://localhost:${port}/">`;
+    return htmlToAmp(html).then(actual => { t.is(actual, expected); });
+  });
+});
+
+test('handle image without src & without width/height', t => {
+  const html = '<img />';
+  const expected = '<article><figure></figure></article>';
 
   return htmlToAmp(html)
     .then(amp => { t.is(amp, expected); });
